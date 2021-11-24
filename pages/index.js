65f22adc880116676685ba25a4405bdcfce9ea7b
Jeremy Mah <jeremymah2000@hotmail.com>
2021-11-24 09:44:17 -0700

    Accidently changed login in stead of index

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,49 @@
-import NavBar from '../components/navbar'
-import Brand from './../components/branding'
+import firebase from "../libs/firebase"
+import { useState } from "react";
+import { useRouter } from "next/router";
+import useSWR from 'swr';
 
-export default function Home() {
-   
+import styled from "styled-components";
+import { Button } from 'ui/buttons'
+import {List, ListItem} from 'classcomp/Present'
+const Wrapper = styled.div`
+  padding: 6rem 0 0;
+  text-align: center;
+  button{
+    width: 40%;
+  }
+  p{
+    font-size: 4.25rem;
+  }
+`;
+// Presentational Component -> Dumb
+//    templating   render data+temp
+// a Smart Component has logic
+//    template render data+temp   code    listeners
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+function Index(props) {
+  //console.log(firebase)
+  const [isValidUser, setValidUser] = useState(false)
+  const router = useRouter()
+
+  function onRequestSignIn (){
+    // firebase loging logic promise
+    // user, error
+    // user dashboard  --> protected route (only valid users can access the dashboard)
+    setValidUser(true)
+  }
+
+  if(isValidUser){
+    router.push('http://www.apple.ca')
+  }
+
   return (
-     <div>
-       <NavBar/>
-       <Brand title="Home Page" tagline="directory app start"/>
-
-     </div>
-     
-       
-    
+    <Wrapper>
+      <Button onClick={onRequestSignIn}>Login</Button>
+    </Wrapper>
   )
 }
+
+export default Index
--- pages/login.js
+++ pages/login.js
@@ -1,49 +1,21 @@
-import firebase from "../libs/firebase"
-import { useState } from "react";
-import { useRouter } from "next/router";
-import useSWR from 'swr';
+// Third Pary Modules
 
-import styled from "styled-components";
-import { Button } from 'ui/buttons'
-import {List, ListItem} from 'classcomp/Present'
-const Wrapper = styled.div`
-  padding: 6rem 0 0;
-  text-align: center;
-  button{
-    width: 40%;
-  }
-  p{
-    font-size: 4.25rem;
-  }
-`;
-// Presentational Component -> Dumb
-//    templating   render data+temp
-// a Smart Component has logic
-//    template render data+temp   code    listeners
+//  Nextjs/Framework Modules
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+// Custom Components
+import NavBar from '../components/navbar'
+import Brand from '../components/branding'
 
-function Index(props) {
-  //console.log(firebase)
-  const [isValidUser, setValidUser] = useState(false)
-  const router = useRouter()
-
-  function onRequestSignIn (){
-    // firebase loging logic promise
-    // user, error
-    // user dashboard  --> protected route (only valid users can access the dashboard)
-    setValidUser(true)
-  }
-
-  if(isValidUser){
-    router.push('http://www.apple.ca')
-  }
+export default function Home() {
 
   return (
-    <Wrapper>
-      <Button onClick={onRequestSignIn}>Login</Button>
-    </Wrapper>
+     <div>
+       <NavBar/>
+       <Brand title="Login Page" tagline="login to firebase"/>
+
+     </div>
+
+
+
   )
 }
-
-export default Index
